6588e15050358ca9d3701fc0fe6f4f6a2faee78f
nismodao <nam.d.dao@gmail.com>
2016-05-12 20:44:28 -0700

    Page now renders if already in archive, or user is sent to loading page.

--- web/request-handler.js
+++ web/request-handler.js
@@ -45,20 +45,44 @@ exports.handleRequest = function (request, response) {
   if (request.method === 'POST' && request.url === '/') {
     var data = '';
     var urlList = '';
-    response.writeHead(302, head.headers);
+    //response.writeHead(302, head.headers);
     //console.log(response);
     request.on('data', function(chunk) {
       data += chunk;
     });
     request.on('end', function() {
-      console.log('Upon end data', data);
-      console.log('writeFile path is ', __dirname + '/archives/sites.txt');
-      urlList += data.slice(4) + '\n';
+      urlList += data.slice(4);
+      console.log('Upon end data', urlList);
       fs.writeFile(archive.paths.list, urlList, function(err) {
-        response.end();
+        archive.isUrlArchived(urlList, function(bool) { 
+          //console.log('Result is', result);
+          if (bool) {
+          //render because page exists
+          //use a method that will redirect the page to the inputed urlList
+            var f = archive.paths.archivedSites + '/' + urlList;
+            console.log('f is', f);
+            fs.readFile(f, 'utf8', function (err, data) {
+              if (data) {
+                console.log('Data from readfile is', data);
+
+                var headers = {'Content-type': mimeTypes['.html']};
+                response.writeHead(302, head.headers); //do we need to change the status code?
+                response.end(data); 
+              }
+            });
+          } else {
+            response.writeHead(302, {'Location': 'http://127.0.0.1:8080/loading.html'});
+            response.end();
+          }    
+            //send them to loading.html
+      
+        });
+
       });
     });
   }
+
+
 };
 
 
@@ -72,3 +96,11 @@ exports.handleRequest = function (request, response) {
  //res.end(archive.paths.list);
 
 
+        //look at the archives/sites directory
+        //   if (archive.isUrlArchived(urlList, function(bool){
+        //     return bool;
+        //   })  {
+        //     //render site
+        //   }
+        // }
+          //else send them to the loading page
